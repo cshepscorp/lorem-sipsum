d7689a0ffe792e78e000bb875091c29c2aa52ae3
cshepscorp <sheppard.christy@gmail.com>
2021-10-07 18:03:00 -0400

    fixed fetch for TM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var searchFormEl = document.querySelector("#search-form-events"); // search area's form id
+
 // YOUR CODE HERE
 var searchTerm = document.querySelector("#searchTerm");
 
@@ -13,36 +13,36 @@ var loadEventsByCity = function() {
   // var apiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN&q=' + theirSearch + '&rating=' + rating;
   var brewApiUrl = 'https://api.openbrewerydb.org/breweries/search?query=' + theirSearch;
 
-  // fetch(brewApiUrl)
-  //     .then(function(response) {
-  //       return response.json();
-  //     })
-  //     .then(function(response) {
-  //       // brewContainer.innerHTML = '';
-  //       // Create a variable that will select the <div> where the GIF will be displayed
-  //       var brewHeadline = document.createElement('h3');
+  fetch(brewApiUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        // brewContainer.innerHTML = '';
+        // Create a variable that will select the <div> where the GIF will be displayed
+        var brewHeadline = document.createElement('h3');
         
-  //       for(var i = 0; i < response.length; i++) {
-  //         // console.log(response[i]);
-  //         var brewContainer = document.querySelector("#brewery-response-container");
+        for(var i = 0; i < response.length; i++) {
+          // console.log(response[i]);
+          var brewContainer = document.querySelector("#brewery-response-container");
           
-  //         var brewSearchListItem = document.createElement('div');
-  //         brewSearchListItem.classList ="card";
+          var brewSearchListItem = document.createElement('div');
+          brewSearchListItem.classList ="card";
 
-  //         var brewSearchReturn = document.createElement('a');
-  //         brewSearchReturn.setAttribute('href', response[i].website_url);
-  //         brewSearchReturn.setAttribute('type', 'submit');
-  //         brewSearchReturn.setAttribute('target', '_blank');
-  //         brewSearchReturn.textContent = response[i].name;
-  //         console.log(brewSearchReturn + 'is bse');
-  //         brewContainer.append(brewSearchListItem);
-  //         brewSearchListItem.append(brewSearchReturn);
+          var brewSearchReturn = document.createElement('a');
+          brewSearchReturn.setAttribute('href', response[i].website_url);
+          brewSearchReturn.setAttribute('type', 'submit');
+          brewSearchReturn.setAttribute('target', '_blank');
+          brewSearchReturn.textContent = response[i].name;
+          console.log(brewSearchReturn + 'is bse');
+          brewContainer.append(brewSearchListItem);
+          brewSearchListItem.append(brewSearchReturn);
 
-  //         brewContainer.append(brewSearchReturn);
-  //         searchTerm.value = '';
-  //       };
+          brewContainer.append(brewSearchReturn);
+          searchTerm.value = '';
+        };
 
-  //     });
+      });
 
       var tmApiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?sort=date,asc&city='
       + theirSearch + tmApi;
@@ -58,7 +58,6 @@ var loadEventsByCity = function() {
             console.log('is this even running?');
             for(var i = 0; i < response._embedded.events.length; i++) {
               var univContainer = document.querySelector("#event-response-container");
-              univContainer.textContent = 'testing';
 
               var univSearchReturnListContainer = document.createElement('div');
               univSearchReturnListContainer.classList = "col s12 m4";
@@ -106,4 +105,4 @@ var loadEventsByCity = function() {
 
 console.log('is this loading');
 
-searchFormEl.addEventListener("submit", loadEventsByCity);
+// loadEventsByCity();
